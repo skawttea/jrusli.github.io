59622b29b8cc8807b55e3c158664b45f4c577a21
joshua <joshuarusli@gmail.com>
2025-08-28 15:29:29 -0700

    done

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import ToolBar from './assets/ToolBar';
-import IntroVideo from './assets/components/IntroVideo';
-import WorkPage from './assets/components/WorkPage';
-import AboutPage from './assets/components/AboutPage';
-import ContactPage from './assets/components/ContactPage';
-
-import './App.css';
+import React from 'react';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <ToolBar />
-        <Routes>
-          <Route path="/" element={<IntroVideo />} />
-          <Route path="/Home" element={<IntroVideo />} />
-          <Route path="/work" element={<WorkPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-        </Routes>
-      </div>
-    </Router>
-  );
+  return <h1>It works!</h1>;
 }
 
 export default App;
